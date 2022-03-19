c8f38755c4cf2f29537f3e8739c018cf7d577607
GoMustangs <jmaier@northportschools.org>
2022-03-19 00:11:25 -0700

    trying again

---
+++ basketballAverages.py
@@ -0,0 +1,7 @@
+game1 = 13
+game2 = 18
+game3 = 21
+game4 = 17
+game5 = 31
+average = ((13+18+21+17+31)/5)
+print(average)
---
+++ tipCalculator.py
@@ -0,0 +1,6 @@
+bill = 47.56
+tip = bill*.10
+total = bill + tip
+print("bill",bill)
+print("tip",tip)
+print("total",total)
